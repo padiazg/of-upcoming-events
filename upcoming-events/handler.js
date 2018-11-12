99451e6decce9e0c998ebd4655f38b7cfc364f3b
Patricio Diaz <padiazg@gmail.com>
2018-11-11 22:21:03 -0300

    First operational version
    Signed-off-by: Patricio Diaz <padiazg@gmail.com>

---
+++ upcoming-events/handler.js
@@ -0,0 +1,15 @@
+"use strict"
+const fs = require("fs");
+const path = require('path');    
+const filePath = path.join(__dirname, '/html/timeline-vis.html');
+
+module.exports = (context, callback) => {
+  fs.readFile(filePath, (err, html) => {
+    if (err) {
+      return callback(err, undefined);
+
+    }
+    callback(undefined, html);
+  })
+
+}
